622222b0a7cb1545ae410c8a506eb8f34b35f6d4
topes1 <banha218@gmail.com>
2021-07-18 12:29:54 +0100

    Removed OnePlus file manager

--- apps/Android.mk
+++ apps/Android.mk
@@ -24,16 +24,6 @@ LOCAL_OVERRIDES_PACKAGES := Photos GalleryGo Gallery3d gallery3d gallery Gallery
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := OnePlusFileManager
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := system/priv-app/OnePlusFileManager/OnePlusFileManager.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := OnePlusCamera
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := system/priv-app/OnePlusCamera/OnePlusCamera.apk
